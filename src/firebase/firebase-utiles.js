aa7386dc19e51e219824779e71dd5bacf4bd4b4b
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-12-15 18:35:51 +0200

    added firebase

--- src/firebase/firebase-utiles.js
+++ src/firebase/firebase-utiles.js
@@ -3,17 +3,42 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAi27iJDCeBdpjIsyLfS0qRv_AeYb8eNLM",
-	authDomain: "social-module-1b719.firebaseapp.com",
-	projectId: "social-module-1b719",
-	storageBucket: "social-module-1b719.appspot.com",
-	messagingSenderId: "211192966317",
-	appId: "1:211192966317:web:559277e60095c60b692a24",
+	apiKey: "AIzaSyBEwhRSlLnUVM2L18plX7ru_ZOO3pF6l-8",
+	authDomain: "social-app-52e71.firebaseapp.com",
+	projectId: "social-app-52e71",
+	storageBucket: "social-app-52e71.appspot.com",
+	messagingSenderId: "656511761256",
+	appId: "1:656511761256:web:81816aad35d292ec9334dd",
 };
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+
+	const userRef = db.doc(`users/${userAuth.uid}`);
+
+	const snapShot = await userRef.get();
+
+	if (!snapShot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData,
+			});
+		} catch (error) {
+			console.log("error creating user", error.message);
+		}
+	}
+
+	return userRef;
+};
+
+export const auth = firebaseApp.auth();
+export const db = firebaseApp.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: "select_account" });
