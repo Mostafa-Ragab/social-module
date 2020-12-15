ca2c6d65d07e7be5a6dc5b1401651f7bf1db33f0
mostafa ragab <49085627+Mostafa-Ragab@users.noreply.github.com>
2020-12-15 18:36:41 +0200

    added files into app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
+import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Sidebar from "./components/Sidebar/Sidebar";
 import { connect } from "react-redux";
 import SignInSide from "./components/Login/SignInSide";
+import { setCurrentUser } from "./redux/user/user-actions";
+import { auth, createUserProfileDocument } from "./firebase/firebase-utiles";
+import Widgets from "./components/Widgets/Widgets";
+import Feed from "./components/feed/Feed";
 
-function App(user) {
-	console.log(user);
+class App extends React.Component {
+	unsubscribeFromAuth = null;
 
-	return (
-		<div className="App">
-			{user ? (
-				<SignInSide />
-			) : (
-				<>
-					<Header />
-					<Sidebar />
-				</>
-			)}
-		</div>
-	);
+	componentDidMount() {
+		const { setCurrentUser } = this.props;
+
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+
+				userRef.onSnapshot((snapShot) => {
+					setCurrentUser({
+						id: snapShot.id,
+						...snapShot.data(),
+					});
+				});
+			}
+
+			setCurrentUser(userAuth);
+		});
+	}
+
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
+
+	render() {
+		return (
+			<div className="App">
+				{this.props.currentUser ? (
+					<>
+						<Header />
+						<div className="app__body">
+							{/* <Sidebar /> */}
+							<Feed />
+							<Widgets />
+						</div>
+					</>
+				) : (
+					<SignInSide />
+				)}
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = ({ user }) => ({
-	user: user.currentUser,
+	currentUser: user.currentUser,
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => ({
+	setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
